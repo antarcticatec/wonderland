7bc6c85d4ebac2d7ba9bbd607b054fdc87915205
jeongho <jeongho@users.noreply.github.com>
2020-12-29 18:08:24 +0900

    update wonderland.py

--- wonderland.py
+++ wonderland.py
@@ -1,2 +1,12 @@
-print("Hello, Wonderland!")
+# -*- coding: utf-8 -*-
+# Copyright (c) Antarctica Technologies, Inc. and its affiliates.
 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+def main():
+    print("Hello, Wonderland!")
+
+
+if __name__ == "__main__":
+    main()
